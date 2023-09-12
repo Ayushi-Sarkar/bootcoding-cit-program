93077dd175f38f53728a50b42f3d329ade33f27a
Ayushi-Sarkar <144773215+Ayushi-Sarkar@users.noreply.github.com>
2023-09-12 06:33:58 -0400

    Add files via upload
    java codes

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+public class Arithmetic{
+public static void main(String args[])
+{
+int a=32,b=8;
+
+int add,sub,mul,div;
+add=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+System.out.println("addition:"+add);
+System.out.println("subtraction:"+sub);
+System.out.println("multiplication:"+mul);
+System.out.println("division:"+div);
+}
+}
+
---
+++ Ifelse.java
@@ -0,0 +1,14 @@
+public class Ifelse{
+public static void main (String args[])
+{
+int a=23;
+if(a%2==0)
+{
+System.out.println(a+"is an even number");
+}
+else
+{
+System.out.println(a+"is an odd numkber");
+}
+}
+}
